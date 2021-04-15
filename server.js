10b8de8d39a9a7e55b63bbd059d490f18ead3f97
bekhzod-ab <bekhzod.ab@gmail.com>
2021-04-15 11:41:50 +0200

    staged with react

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config()
 const cors = require("cors")
 const mongoose = require("mongoose")
 const express = require("express")
+const path = require("path")
 const app = express()
 const port = process.env.PORT || 4004
 const {SaveProduct, DeleteProduct, MarkDone, PrintAll } = require("./productsModule.js")
@@ -22,22 +23,26 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 
-app.get("/products",async(req,res) => {
+
+app.get("/api/products",async(req,res) => {
     const products = await PrintAll()
     res.status(200).json(products)
 })
 
-app.post("/save", async(req,res) => {   
+app.post("/api/save", async(req,res) => {   
     await SaveProduct(req.body.input)
     res.send("added succesefuly")
 })
 
-app.put("/products/:id", async(req,res) => {
+app.put("/api/products/:id", async(req,res) => {
     await MarkDone(req.params.id)
     res.status(200).send()
 })
 
-app.delete("/products/:id", async(req,res) => {
+app.delete("/api/products/:id", async(req,res) => {
     await DeleteProduct(req.params.id)
     res.status(200).send()
 })
+
+app.use(express.static(path.join(__dirname, "build")))
+app.use("/", (req,res) => res.sendFile(path.join(__dirname, "build","index.html")))
